58b35da899789c40ea4097f5bbe1e977c8584817
Arsalan Shah <manactric@gmail.com>
2021-02-05 17:34:30 +0500

    1.1

--- plugins/default/mentions/js.php
+++ plugins/default/mentions/js.php
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	if(typeof $.fn.atwho !== 'undefined'){					   
 		$('.comment-box').atwho({
  	   		at: "@",
+			limit:100,
  	  		insertTpl: '<span class="mentions">@${username}</span>',
  	   		displayTpl: "<li><img src='${imageurl}' height='20' width='20'/> ${first_name} ${last_name}</li>",	
  	   		data: Ossn.site_url+"mentions_picker",
