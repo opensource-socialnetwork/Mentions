2bd4b1451323f75b5b535aeb8352dbb99e697785
lianglee <manactric@gmail.com>
2025-06-25 20:14:53 +0500

    3.4

--- plugins/default/mentions/js.php
+++ plugins/default/mentions/js.php
@@ -40,31 +40,6 @@ function observeContentEditableCleanup(selector = '[contenteditable=true]') {
         element.__cleanupObserved = true;
     });
 }
-function observeContentEditableCleanup(selector = '[contenteditable=true]') {
-    // Loop through all matching elements
-    document.querySelectorAll(selector).forEach(function (element) {
-        // Skip if already observed (optional: to avoid duplicate observers)
-        if (element.__cleanupObserved) return;
-
-        var observer = new MutationObserver(function (mutationsList) {
-            mutationsList.forEach(function (mutation) {
-                mutation.addedNodes.forEach(function (node) {
-                    if (node.nodeType === 1 && node.tagName === 'SPAN') {
-                        node.outerHTML = node.innerHTML; // unwrap <span>
-                    }
-                });
-            });
-        });
-
-        observer.observe(element, {
-            childList: true,
-            subtree: true
-        });
-
-        // Mark as observed
-        element.__cleanupObserved = true;
-    });
-}
 //bug in chrome that re-creates old element after removing using backspace
 //https://stackoverflow.com/questions/31207738/contenteditable-re-creates-deleted-child-elements
 $(document).ready(function() {
